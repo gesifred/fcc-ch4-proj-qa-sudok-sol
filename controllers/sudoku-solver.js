7587652fac2947fe8aca0a8b9b776b081e74e3bf
Wilfred V <gesifred@gmail.com>
2023-05-14 01:47:17 -0400

    create full map of possible solutions

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -17,23 +17,23 @@ class SudokuSolver {
     for (let c = 0; c < 9; c++) {
       total += chosenRow[c] === "." ? 0 : Number(chosenRow[c]);
     }
-    return total <= 45 ;
+    return total <= 45;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
     let rows = puzzleString.match(/.{9}/g); //array with 9 rows
-    let total=0;
+    let total = 0;
     let chosenCol = [];
-    for(let r=0;r<9;r++){
-      if (Number(rows[r][column])=== Number(value)) return false;
+    for (let r = 0; r < 9; r++) {
+      if (Number(rows[r][column]) === Number(value)) return false;
       chosenCol.push(Number(rows[r][column]));
-      total+= rows[r][column] === "." ? 0 : Number(rows[r][column]);
+      total += rows[r][column] === "." ? 0 : Number(rows[r][column]);
     }
     //if(chosenCol.indexOf(Number(value))!==chosenCol.lastIndexOf(Number(value))) {
-    if(chosenCol.includes(Number(value))) {
+    if (chosenCol.includes(Number(value))) {
       return false;
     } else {
-      total+=value;
+      total += value;
       return total <= 45;
     }
   }
@@ -41,22 +41,44 @@ class SudokuSolver {
   checkRegionPlacement(puzzleString, row, column, value) {
     let rows = puzzleString.match(/.{9}/g); //array with 9 rows
     let chosenRegion = [];
-    let windowRow = parseInt(row/3);
-    let windowCol = parseInt(column/3);
-    let total=0;
-    for (let i=0;i<3;i++){
-      for(let j=0;j<3;j++){
-        if (Number(rows[i+windowRow*3][j+windowCol*3])=== Number(value)) return false;
-        chosenRegion.push(Number(rows[i+windowRow*3][j+windowCol*3]));
-        total+= rows[i][j] === "." ? 0 : Number(rows[i][j]);
+    let windowRow = parseInt(row / 3);
+    let windowCol = parseInt(column / 3);
+    let total = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (Number(rows[i + windowRow * 3][j + windowCol * 3]) === Number(value)) return false;
+        chosenRegion.push(Number(rows[i + windowRow * 3][j + windowCol * 3]));
+        total += rows[i][j] === "." ? 0 : Number(rows[i][j]);
       }
     }
-    if(chosenRegion.includes(Number(value))) {
+    if (chosenRegion.includes(Number(value))) {
       return false;
     } else {
-      total+=value;
+      total += value;
       return total <= 45;
     }
+  }
+  calculateRowColReg(puzzleString, row, col, value) {
+    return this.checkRowPlacement(puzzleString, row, col, value) &&
+      this.checkColPlacement(puzzleString, row, col, value) &&
+      this.checkRegionPlacement(puzzleString, row, col, value);
+  }
+
+  createMapOfSolutions(puzzleString) {
+    let rows = puzzleString.match(/.{9}/g); //array with 9 rows
+    const possibleSolutionsMap = new Map();
+    for (let i = 0; i < 9; i++) {
+      for (let j = 0; j < 9; j++) {
+        const possibleSolutions = [];
+        for (let n = 1; n <= 9; n++) {
+          if (rows[i][j] === "." && calculateRowColReg(puzzleString, i, j, n)) {
+            possibleSolutions.push(Number(n));
+          }
+        }
+        possibleSolutionsMap.set(i * 9 + j, possibleSolutions);
+      }
+    }
+    return possibleSolutionsMap;
   }
 
   solve(puzzleString) {
