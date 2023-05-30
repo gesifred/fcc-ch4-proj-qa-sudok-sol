d576ecf59d23b0b73a8b97bf0033f9729abbc92a
Wilfred V <gesifred@gmail.com>
2023-05-29 23:16:39 -0400

    simplifying check methods and adding first tests

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -3,79 +3,77 @@ class SudokuSolver {
   validate(puzzleString) {
     let valid = true;
     puzzleString.split("").forEach(el => {
-      valid &= (el == "." || el <= 9 || el >= 1) ? true : false;
+      valid &= (el === "." || (el <= 9 && el >= 1));
     });
-    return valid && puzzleString.length == 81;
+    return (valid && puzzleString.length == 81);
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
     let rows = puzzleString.match(/.{9}/g); //array with 9 rows
     let chosenRow = rows[row].split("");
-    let total = 0;
     if (chosenRow.indexOf(String(value)) >= 0) return false;
-    chosenRow[column] = value;
-    for (let c = 0; c < 9; c++) {
-      total += chosenRow[c] === "." ? 0 : Number(chosenRow[c]);
-    }
-    return total <= 45;
+    return true;
   }
 
   checkColPlacement(puzzleString, row, column, value) {
     let rows = puzzleString.match(/.{9}/g); //array with 9 rows
-    let total = 0;
-    let chosenCol = [];
     for (let r = 0; r < 9; r++) {
       if (Number(rows[r][column]) === Number(value)) return false;
-      chosenCol.push(Number(rows[r][column]));
-      total += rows[r][column] === "." ? 0 : Number(rows[r][column]);
     }
-    //if(chosenCol.indexOf(Number(value))!==chosenCol.lastIndexOf(Number(value))) {
-    if (chosenCol.includes(Number(value))) {
-      return false;
-    } else {
-      total += value;
-      return total <= 45;
-    }
+    return true;
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
     let rows = puzzleString.match(/.{9}/g); //array with 9 rows
-    let chosenRegion = [];
     let windowRow = parseInt(row / 3);
     let windowCol = parseInt(column / 3);
-    let total = 0;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
         if (Number(rows[i + windowRow * 3][j + windowCol * 3]) === Number(value)) return false;
-        chosenRegion.push(Number(rows[i + windowRow * 3][j + windowCol * 3]));
-        total += rows[i][j] === "." ? 0 : Number(rows[i][j]);
       }
     }
-    if (chosenRegion.includes(Number(value))) {
-      return false;
-    } else {
-      total += value;
-      return total <= 45;
-    }
+    return true;
   }
-  calculateRowColReg(puzzleString, row, col, value) {
+
+  checkValid(puzzleString, row, col, value) {
     return this.checkRowPlacement(puzzleString, row, col, value) &&
       this.checkColPlacement(puzzleString, row, col, value) &&
       this.checkRegionPlacement(puzzleString, row, col, value);
   }
 
-  createMapOfSolutions(puzzleString) {
+  findPositionToFill(puzzleString) {
+    return puzzleString.indexOf(".");
+  }
+
+  findPossibleSolutions(puzzleString, position) {
+    let rows = puzzleString.match(/.{9}/g);
+    let row = parseInt(position / 9);
+    let col = (position % 9);
+    let solutions = [];
+    for (let n = 1; n <= 9; n++) {
+      if (rows[row][col] === "." && this.checkValid(puzzleString, row, col, n)) {
+        solutions.push(Number(n));
+      }
+    }
+    return solutions;
+  }
+
+  findMapOfSolutions(puzzleString) {
     let rows = puzzleString.match(/.{9}/g); //array with 9 rows
     const possibleSolutionsMap = new Map();
     for (let i = 0; i < 9; i++) {
       for (let j = 0; j < 9; j++) {
         const possibleSolutions = [];
         for (let n = 1; n <= 9; n++) {
-          if (rows[i][j] === "." && calculateRowColReg(puzzleString, i, j, n)) {
+          if (rows[i][j] === "." && this.checkValid(puzzleString, i, j, n)) {
             possibleSolutions.push(Number(n));
           }
         }
-        possibleSolutionsMap.set(i * 9 + j, possibleSolutions);
+        if (rows[i][j] === "." && possibleSolutions.length == 0) {
+          possibleSolutionsMap.set(i * 9 + j, null);
+        } else {
+          possibleSolutionsMap.set(i * 9 + j, possibleSolutions);
+        }
       }
     }
     return possibleSolutionsMap;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,7 +3,46 @@ const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
 let solver = new Solver();
+const puzzles = require('../controllers/puzzle-strings.js');
 
 suite('Unit Tests', () => {
-
+    test("Logic handles a valid puzzle string of 81 characters", function () {
+		assert.equal(true , solver.validate("..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."));
+	});
+    test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", function () {
+		assert.equal(false , solver.validate("._.9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."));
+	});
+	
+    test("Logic handles a puzzle string that is not 81 characters in length", function () {
+		assert.equal(false , solver.validate("1..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6.."));
+	});
+	
+    test("Logic handles a valid row placement", function () {
+		assert.equal(true , solver.checkRowPlacement("..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",1,2,1));
+	});
+	
+    test("Logic handles an invalid row placement", function () {
+		assert.equal(false , solver.checkRowPlacement("..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",1,2,4));
+	});
+	
+    test("Logic handles a valid column placement", function () {
+		assert.equal(true , solver.checkColPlacement("..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",2,5,2));
+	});
+    test("Logic handles an invalid column placement", function () {
+		assert.equal(false , solver.checkColPlacement("..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..",2,5,9));
+	});
+	
+    test("Logic handles a valid region (3x3 grid) placement", function () {
+		assert.equal(true , solver.checkRegionPlacement(".7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6",0,2,1));
+	});
+    test("Logic handles an invalid region (3x3 grid) placement", function () {
+		assert.equal(false , solver.checkRegionPlacement(".7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6",0,2,2));
+	});
+	/*	
+    test("Valid puzzle strings pass the solver", function () {
+	});
+	test("Invalid puzzle strings fail the solver", function () {
+	});
+    test("Solver returns the expected solution for an incomplete puzzle", function () {
+	});*/
 });
